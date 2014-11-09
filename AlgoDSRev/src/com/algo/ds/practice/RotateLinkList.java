a7f5aa7943ce1e351d4f8ce67e8969ce78e7efd4
sumitdanish <sumit.polite@gmail.com>
2014-11-09 16:06:36 +0530

    a

---
+++ AlgoDSRev/src/com/algo/ds/practice/RotateLinkList.java
@@ -0,0 +1,121 @@
+package com.algo.ds.practice;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class RotateLinkList {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		
+		try
+		{
+			Scanner sc = new Scanner(new File("link.txt"));
+		    String[] a = sc.next().split(",");
+		    Node list = null;
+		    CreateLinkList cl = new CreateLinkList();
+		    for(String s : a)
+		    {
+			     list = cl.createListRecur(Integer.parseInt(s),list);
+		    }
+		    sc.close();
+		    RotateLinkList rotate = new RotateLinkList();
+		    cl.print(list);
+		    System.out.println("\n_____________________________________\n");
+		    Node li = rotate.rotateALterNate(list,3);
+		    cl.print(li);
+		}
+		catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}
+
+	}
+
+	private Node rotateList(Node list,int k)
+	{
+		try
+		{
+			Node n=list;
+			while(n!=null&&k>0)
+			{
+				n=n.getNextNode();
+				k--;
+			}
+			if(n==null)
+			{
+				return n;
+			}
+			Node n1=n;
+			while(n.getNextNode()!=null)
+			{
+				n=n.getNextNode();
+			}
+			n.setNextNode(list);
+			list=n1.getNextNode();
+			n1.setNextNode(null);
+			return list;
+		}
+		catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}
+		return null;
+	}
+	private Node rotateALterNate(Node list,int k)
+	{
+		try
+		{
+			Node n = list;
+			Node temp=null;
+			Node n1=null;
+			Node n2=null;
+			int k1=k;
+			while(n!=null)
+			{
+				Node t = null;
+				temp=n.getNextNode();
+				k1=k;
+				while(temp!=null&&k1>0)
+				{
+					n.setNextNode(t);
+					t=n;
+					n=temp;
+					temp=temp.getNextNode();
+					k1--;
+				}
+				if(n1==null)
+				{
+					n1=t;
+					n2=n1;
+				}
+				else
+				{
+					while(n1.getNextNode()!=null)
+					{
+						n1=n1.getNextNode();
+					}
+					n1.setNextNode(t);
+					n1=t;
+				}
+				k1=k;
+//				while(temp!=null&&k1>0)
+//				{
+//					temp=temp.getNextNode();
+//					k1--;
+//				}
+				n=temp;
+			}
+			list=n2;
+			return list;
+		}
+		catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}
+		return null;
+	}
+}
